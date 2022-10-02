e513facf4e27fabebaf821fcbe317130f6fc23ce
RTugeau <Rtugeau@mail.umw.edu>
2022-10-02 19:52:54 -0400

    File Input added

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -1,5 +1,12 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class BigNumArithmetic {
-    public static void main(String[] args) {
-        System.out.println("hello!");
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream fileIn = new FileInputStream(args[0]);
+        Scanner scan = new Scanner(fileIn);
+        int num1 = scan.nextInt();
+        System.out.println(num1);
     }
 }
